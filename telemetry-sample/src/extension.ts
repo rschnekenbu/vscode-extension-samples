afa0454d2a9d24cde53ddbd2b27091adfa11eda1
Remi Schnekenburger <rschnekenburger@eclipsesource.com>
2023-04-24 22:39:11 +0200

    adapt telemetry example

--- telemetry-sample/src/extension.ts
+++ telemetry-sample/src/extension.ts
@@ -4,35 +4,55 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "telemetry-sample" is now active!');
 
 
-	const appender: vscode.TelemetryAppender = {
-		ignoreBuiltInCommonProperties: false,
-		logEvent: (eventName, data) => {
-			console.log(`Event: ${eventName}`);
+	const sender: vscode.TelemetrySender = {
+		sendErrorData(error, data) {
+			console.log("===== extension ERROR sender ===== ");
+			console.log(`Exception: ${error}`);
 			console.log(`Data: ${JSON.stringify(data)}`);
 		},
-		logException: (exception, data) => {
-			console.log(`Exception: ${exception}`);
+		sendEventData(eventName, data) {
+			console.log("===== extension EVENT sender ===== ");
+			console.log(`Event: ${eventName}`);
 			console.log(`Data: ${JSON.stringify(data)}`);
 		}
 	};
 
-	const logger = vscode.env.createTelemetryLogger(appender);
-
-	/**
-	 * You can use proposed API here. `vscode.` should start auto complete
-	 * Proposed API as defined in vscode.proposed.<proposalName>.d.ts.
-	 */
+	const logger = vscode.env.createTelemetryLogger(sender);
 
 	const c1 = vscode.commands.registerCommand('extension.logEvent', () => {
-		vscode.window.showInformationMessage('Logged telemetry event!');
-		logger.logUsage('testEvent', { 'testProp': 'testValue' });
+		const data =  { 
+			'testProp': 'testValue',
+			'file path': 'file:///foo/bar',
+			'fake email': 'foo@bar.co',
+			'secret': 'pwd: 126',
+			'not-secret': 'pwd126',
+			'tesProp2' : {
+				'value': undefined,
+				'subprop' : 'prop',
+				'fake email': 'foo@bar.co'
+			}
+		};
+		vscode.window.showInformationMessage('Logged telemetry event!: '+JSON.stringify(data));
+		logger.logUsage('testEvent', data);
 	});
 
 	context.subscriptions.push(c1);
 
 	context.subscriptions.push(vscode.commands.registerCommand('extension.logException', () => {
-		vscode.window.showInformationMessage('Logged telemetry exception!');
-		logger.logError(new Error('Test'), { 'testProp': 'testValue' });
-		logger.logError('testerror', { 'testProp': 'testValue' });
+		const data = { 
+			'testProp': 'testValue',
+			'file path': 'file:///foo/bar',
+			'fake email': 'foo@bar.co',
+			'secret': 'pwd: 126',
+			'not-secret': 'pwd126',
+			'tesProp2' : {
+				'value': undefined,
+				'subprop' : 'prop',
+				'fake email': 'foo@bar.co'
+			}
+		};
+		vscode.window.showInformationMessage('Logged telemetry exception!'+JSON.stringify(data));
+		logger.logError('testerror', data);
 	}));
+
 }
